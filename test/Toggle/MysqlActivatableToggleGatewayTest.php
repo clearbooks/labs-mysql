3ce0a5234427acaea4ae40e2936856daa26c84ac
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-08-13 14:38:39 +0100

    adds a first test

---
+++ test/Toggle/MysqlActivatableToggleGatewayTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Volodymyr
+ * Date: 13/08/2015
+ * Time: 14:38
+ */
+class MysqlActivatableToggleGatewayTest extends PHPUnit_Framework_TestCase
+{
+
+}
