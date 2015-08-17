ae335f6883135be5b4f2535f5af746ce209117e8
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-08-17 15:11:56 +0100

    Spell checks test names.

--- test/Toggle/MysqlActivatableToggleGatewayTest.php
+++ test/Toggle/MysqlActivatableToggleGatewayTest.php
@@ -74,7 +74,7 @@ class MysqlActivatableToggleGatewayTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function givenExistentActivatableToggle_MysqlActivatableToggleGateway_RetusnsExistentToggle()
+    public function givenExistentActivatableToggle_MysqlActivatableToggleGateway_ReturnsExistentToggle()
     {
         $releaseName = 'Test activatable toggle 2';
         $url = 'a helpful url';
@@ -93,7 +93,7 @@ class MysqlActivatableToggleGatewayTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function givenMultipleExistentToggleWithDifferentNames_MysqlActivatableToggleGateway_RetusnsRequestedExistentToggle()
+    public function givenMultipleExistentToggleWithDifferentNames_MysqlActivatableToggleGateway_ReturnsRequestedExistentToggle()
     {
         $releaseName = 'Test activatable toggle 3';
         $url = 'a helpful url';
