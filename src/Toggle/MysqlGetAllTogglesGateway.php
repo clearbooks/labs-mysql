dac6083ccceeff5481e2ddc32d3c54cbddb8d15e
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-08-28 15:43:34 +0100

    Refactoring to remove complexity problems

--- src/Toggle/MysqlGetAllTogglesGateway.php
+++ src/Toggle/MysqlGetAllTogglesGateway.php
@@ -22,13 +22,15 @@ abstract class MysqlGetAllTogglesGateway
         $toggles = [ ];
         foreach ( $data as $row ) {
 
-            $screenshotUrl = isset( $row[ 'screenshot_urls' ] ) ? $row[ 'screenshot_urls' ] : "";
-            $descriptionOfToggle = isset( $row[ 'description_of_toggle' ] ) ? $row[ 'description_of_toggle' ] : "";
-            $descriptionOfFunctionality = isset( $row[ 'description_of_functionality' ] ) ? $row[ 'description_of_functionality' ] : "";
-            $descriptionOfImplementationReason = isset( $row[ 'description_of_implementation_reason' ] ) ? $row[ 'description_of_implementation_reason' ] : "";
-            $descriptionOfLocation = isset( $row[ 'description_of_location' ] ) ? $row[ 'description_of_location' ] : "";
-            $guideUrl = isset( $row[ 'guide_url' ] ) ? $row[ 'guide_url' ] : "";
-            $appNotificationCopyText = isset( $row[ 'app_notification_copy_text' ] ) ? $row[ 'app_notification_copy_text' ] : "";
+            list(
+                $screenshotUrl,
+                $descriptionOfToggle,
+                $descriptionOfFunctionality,
+                $descriptionOfImplementationReason,
+                $descriptionOfLocation,
+                $guideUrl,
+                $appNotificationCopyText
+                ) = $this->setDefaultMarketingInformationForToggle( $row );
 
             $toggles[] = new Toggle(
                 $row[ 'name' ],
@@ -44,6 +46,93 @@ abstract class MysqlGetAllTogglesGateway
             );
         }
         return $toggles;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultMarketingInformationForToggle( $row )
+    {
+        $screenshotUrl = $this->setDefaultForScreenshotUrl( $row );
+        $descriptionOfToggle = $this->setDefaultForDescriptionOfToggle( $row );
+        $descriptionOfFunctionality = $this->setDefaultForDescriptionOfFunctionality( $row );
+        $descriptionOfImplementationReason = $this->setDefaultForDescriptionOfImplementationReason( $row );
+        $descriptionOfLocation = $this->setDefaultForDescriptionOfLocation( $row );
+        $guideUrl = $this->setDefaultForGuideUrl( $row );
+        $appNotificationCopyText = $this->setDefaultForAppNotificationCopyText( $row );
+
+        return array( $screenshotUrl, $descriptionOfToggle, $descriptionOfFunctionality, $descriptionOfImplementationReason, $descriptionOfLocation, $guideUrl, $appNotificationCopyText );
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForScreenshotUrl( $row )
+    {
+        $screenshotUrl = isset( $row[ 'screenshot_urls' ] ) ? $row[ 'screenshot_urls' ] : null;
+        return $screenshotUrl;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForDescriptionOfToggle( $row )
+    {
+        $descriptionOfToggle = isset( $row[ 'description_of_toggle' ] ) ? $row[ 'description_of_toggle' ] : null;
+        return $descriptionOfToggle;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForDescriptionOfFunctionality( $row )
+    {
+        $descriptionOfFunctionality = isset( $row[ 'description_of_functionality' ] ) ? $row[ 'description_of_functionality' ] : null;
+        return $descriptionOfFunctionality;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForDescriptionOfImplementationReason( $row )
+    {
+        $descriptionOfImplementationReason = isset( $row[ 'description_of_implementation_reason' ] ) ? $row[ 'description_of_implementation_reason' ] : null;
+        return $descriptionOfImplementationReason;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForDescriptionOfLocation( $row )
+    {
+        $descriptionOfLocation = isset( $row[ 'description_of_location' ] ) ? $row[ 'description_of_location' ] : null;
+        return $descriptionOfLocation;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForGuideUrl( $row )
+    {
+        $guideUrl = isset( $row[ 'guide_url' ] ) ? $row[ 'guide_url' ] : null;
+        return $guideUrl;
+    }
+
+    /**
+     * @param $row
+     * @return array
+     */
+    protected function setDefaultForAppNotificationCopyText( $row )
+    {
+        $appNotificationCopyText = isset( $row[ 'app_notification_copy_text' ] ) ? $row[ 'app_notification_copy_text' ] : null;
+        return $appNotificationCopyText;
     }
 
 }
