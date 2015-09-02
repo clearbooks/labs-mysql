47ec77e955b59c327e5e6c6d0898160394017e31
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-09-02 08:21:42 +0100

    Makes changes: changes COLLATE from latin1 to utf8mb4. deletes un-used __DIR__."/../vendor..."

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,5 +10,4 @@
 use Clearbooks\Labs\Bootstrap;
 use Clearbooks\Labs\Db\DbDIDefinitionProvider;
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../vendor/clearbooks/labs-php-client-mysql/src/Bootstrap.php";
 Bootstrap::getInstance()->init( [ DbDIDefinitionProvider::class ] );
